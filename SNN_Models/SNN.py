ec547b653892cc4e11365d661aca89b98f8a5d18
Frederik Grüneberg <agent21@alpenjodel.de>
2025-06-06 15:59:29 +0200

    Add small changes

--- SNN_Models/SNN.py
+++ SNN_Models/SNN.py
@@ -9,6 +9,8 @@ from snntorch import surrogate, utils
 device = (
     torch.device("cuda")
     if torch.cuda.is_available()
+    else torch.device("mps")
+    if torch.backends.mps.is_available()
     else torch.device("cpu")
 )
 
