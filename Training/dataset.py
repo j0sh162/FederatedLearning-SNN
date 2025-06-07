851ed833f2d47e1c49bb8ba6e7c6c7821aa81c03
ThomasVroom <thomasvroom@live.nl>
2025-06-07 18:31:21 +0200

    remove validation sets and collate_fn from dataset loading

--- Training/dataset.py
+++ Training/dataset.py
@@ -148,21 +148,16 @@ def load_dataset(
     path,
     num_partitions: int,
     batch_size: int,
-    val_ratio: float = 0.1,
+    seed: int,
     non_iid=True,
 ):
-    """val_ratio is used to vary data amount among clients"""
-    transform = transforms.ToTensor()
 
     if name == "mnist":
         trainSet, testSet = get_MNISTdataset(path)
-        collate_fn = None
     elif name == "cifar10":
         trainSet, testSet = get_CIFARdataset(path)
-        collate_fn = None
     elif name == "NMNIST":
         trainSet, testSet = get_NMNIST_dataset(path)
-        collate_fn = tonic.collation.PadTensors(batch_first=False)
     else:
         raise ValueError(f"Unsupported dataset: {name}")
 
@@ -173,46 +168,25 @@ def load_dataset(
         num_images = len(trainSet) // num_partitions
         partition_length = [num_images] * num_partitions
         trainsets = random_split(
-            trainSet, partition_length, torch.Generator().manual_seed(42)
+            trainSet, partition_length, torch.Generator().manual_seed(seed)
         )  # For repetition
 
     # partition into train and validation sets
     trainingList = []
-    validationList = []
     for partition in trainsets:
-        total = len(partition)
-        validation_length = int(val_ratio * total)
-        train_length = total - validation_length
-
-        for_training, for_validation = random_split(
-            partition,
-            [train_length, validation_length],
-            generator=torch.Generator().manual_seed(42),
-        )
-
         trainingList.append(
             DataLoader(
-                for_training,
+                partition,
                 batch_size=batch_size,
                 shuffle=True,
                 num_workers=2,
-                collate_fn=collate_fn,
-            )
-        )
-        validationList.append(
-            DataLoader(
-                for_validation,
-                batch_size=batch_size,
-                shuffle=False,
-                num_workers=2,
-                collate_fn=collate_fn,
             )
         )
 
     print("BATCH SIZE: ", batch_size)
     testList = DataLoader(
         testSet,
-        batch_size=batch_size,  # originally 128
-        collate_fn=collate_fn,
+        batch_size=batch_size,
+        shuffle=False
     )
-    return trainingList, validationList, testList
+    return trainingList, testList
