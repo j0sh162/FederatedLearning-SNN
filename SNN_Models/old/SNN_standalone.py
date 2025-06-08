ec5d49a60ca338b15161bfc7c7a70ba24f6fd6ee
Frederik Grüneberg <82397734+fregru@users.noreply.github.com>
2025-06-08 13:25:12 +0200

    Add first fl experiment results

--- SNN_Models/old/SNN_standalone.py
+++ SNN_Models/old/SNN_standalone.py
@@ -139,10 +139,13 @@ test_loss, test_acc = test(snn_net, test_loader, device)
 hist.append([epoch, batch_number, test_loss, test_acc, "test"])
 print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc * 100:.2f}%")
 
+torch.save(
+    snn_net.net.state_dict(),
+    f"runs/SurrogateGradient_centralized/{args.seed}/snn_net.pt",
+)
 
 writer.flush()
 writer.close()
-torch.save(snn_net.net.state_dict(), "snn_net.pt")
 
 # fig = plt.figure(facecolor="w")
 # df = pd.DataFrame(hist, columns=["epoch", "batch", "Loss", "Acc", "Set"])
