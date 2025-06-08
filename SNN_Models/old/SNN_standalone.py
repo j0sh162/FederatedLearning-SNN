112bc1972822fdd125d6a77c07e90cd448537056
Frederik Grüneberg <82397734+fregru@users.noreply.github.com>
2025-06-08 09:31:41 +0200

    Change SG to atan

--- SNN_Models/old/SNN_standalone.py
+++ SNN_Models/old/SNN_standalone.py
@@ -21,6 +21,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from SNN_Models import SNN
 from SNN_Models.SNN_utils import test
+from Training import dataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=0)
@@ -40,22 +41,13 @@ sensor_size = tonic.datasets.NMNIST.sensor_size
 
 # Denoise removes isolated, one-off events
 # time_window
-frame_transform = transforms.Compose(
-    [
-        transforms.Denoise(filter_time=10000),
-        transforms.ToFrame(sensor_size=sensor_size, n_time_bins=20),
-    ]
-)
-
-trainset = tonic.datasets.NMNIST(
-    save_to="./data", transform=frame_transform, train=True
-)
-testset = tonic.datasets.NMNIST(
-    save_to="./data", transform=frame_transform, train=False
-)
+trainset, testset = dataset.get_NMNIST_dataset("./data")
 
 transform = tonic.transforms.Compose(
-    [torch.from_numpy, torchvision.transforms.RandomRotation([-10, 10])]
+    [
+        # torch.from_numpy,
+        # torchvision.transforms.RandomRotation([-10, 10]),
+    ]
 )
 
 cached_trainset = MemoryCachedDataset(trainset, transform=transform)
@@ -68,13 +60,15 @@ train_loader = DataLoader(
     cached_trainset,
     batch_size=batch_size,
     collate_fn=tonic.collation.PadTensors(batch_first=False),
-    shuffle=True,
+    shuffle=False,
 )
 test_loader = DataLoader(
     cached_testset,
     batch_size=batch_size,
     collate_fn=tonic.collation.PadTensors(batch_first=False),
 )
+
+# train_loader, test_loader = dataset.load_dataset()
 
 device = (
     torch.device("cuda")
@@ -85,13 +79,8 @@ device = (
 )
 print(f"Using device: {device}")
 
-snn_net = SNN.Net(
-    input_shape=reversed(sensor_size),
-    num_hidden=None,
-    num_output=None,
-    spike_grad=surrogate.atan(),
-    beta=0.5,
-)
+snn_net = SNN.Net()
+
 if False:  # os.path.exists("snn_net.pt"):
     print("Loading existing model weights")
     # Load the model weights
@@ -99,7 +88,7 @@ if False:  # os.path.exists("snn_net.pt"):
         torch.load("snn_net.pt", weights_only=True, map_location=device)
     )
 
-optimizer = torch.optim.AdamW(snn_net.net.parameters(), lr=2e-2, betas=(0.9, 0.999))
+optimizer = torch.optim.AdamW(snn_net.net.parameters(), lr=0.0002, betas=(0.9, 0.999))
 loss_fn = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
 
 num_epochs = args.epochs
